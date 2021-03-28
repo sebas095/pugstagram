d8839bb312be3362f92f00b8c8bff676e3740610
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2021-03-28 14:33:20 -0500

    [ADD] loader svelte

---
+++ src/components/Loader.svelte
@@ -0,0 +1,29 @@
+<style>
+  .Loader {
+    display: grid;
+    place-items: center;
+    height: 80vh;
+  }
+
+  .Loader-container {
+    border: 8px solid white;
+    border-radius: 50%;
+    border-top-color: #999;
+    width: 80px;
+    height: 80px;
+    animation: spin 2s linear infinite;
+  }
+
+  @keyframes spin {
+    0% {
+    transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
+
+<div class="Loader">
+  <div class="Loader-container"></div>
+</div>
